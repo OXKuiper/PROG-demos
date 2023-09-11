0045eb8a56b0c03d4bd244c32ecbfff82e9219b6
OXKuiper <o.x.kuiper@gmail.com>
2023-09-11 10:36:22 +0200

    Prog2 aangepast

--- PROG2.py
+++ PROG2.py
@@ -1,3 +1,17 @@
 # Hier ga ik wat dingen demo'en voor les2...
 # later zijn deze te vinden op github.com/OXKuiper/PROG-demos/
 
+#user_input = input('Wat is je naam?')
+#print('Hoi, je heet: ' + user_input)
+
+user_age = input('Wat is je leeftijd?')
+user_age = int(user_age)
+user_age_times_two = user_age*2
+
+print('Hoi, je bent (keer2) ' + str(user_age_times_two))
+
+
+
+
+
+
