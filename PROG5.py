f0e1447a86d01d3c01d8e0f0bbaa1ad862ea6cad
OXKuiper <o.x.kuiper@gmail.com>
2023-09-21 11:26:39 +0200

    prog 5 voorbeelden

---
+++ PROG5.py
@@ -0,0 +1,8 @@
+
+s1 = 'Hoi Barry'
+s2 = s1.replace('Hoi', 'Hai')
+print(s2)  # geeft 'Hai Barry'
+
+# methode gebruiken op string of op variabele de wijst naar string kan beiden
+s = 'henk'.upper() # in s zit nu 'HENK'
+l = 'dit is een zin'.split() # in l zit ['dit', 'is', 'een', 'zin']
