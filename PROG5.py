e26d7097ab4855d97d13795d11fb569e0b91c1f5
OXKuiper <o.x.kuiper@gmail.com>
2023-09-25 14:25:15 +0200

    PROG5 voorbeelden

--- PROG5.py
+++ PROG5.py
@@ -1,3 +1,5 @@
+### Democode uit les 5.
+
 ### Files openen
 
 # open()
@@ -10,10 +12,17 @@ print(lines)
 # vergeet niet te close()'n! Of gebruik 'with'!
 infile.close()
 
+# open met with voorbeeld:
+with open('Data/voorbeeld2.txt') as infile:
+    lines = infile.readlines()
+    print(lines)
+    # aan het einde doet hij dankzij with close() vanzelf
+
 # Tip, je kan ook naar een file in een andere folder/map.
-# Stel je hebt een projectmap met de mappen pyfiles en data
-# dan kan je vanuit /pyfiles/PROG5.py naar data/voorbeeld.txt met:
+# Stel je hebt een projectmap met de mappen: pyfiles en data
+# dan kan je als je werk in /pyfiles/PROG5.py naar data/voorbeeld.txt met:
 #  open("../data/voorbeeld") . Het stukje "../" zegt zeg maar 'ga een map terug'.
+
 
 ## String methods (handige methoden om strings te bewerken)
 s1 = 'Hoi Barry'
@@ -37,10 +46,9 @@ print(lijst[0:3]) # geeft het nulde-tot-het-3e ding, dus 0e, 1e, 2e.
 
 # # schrijf een for-loop die over een lijst getallen loopt, alle even getallen print
 #
-# lijst = [1,2,3,4,5,6,7,8]
-# #sum(lijst)
+# lijst = [0,1,2,3,4,5,6,7,8]
 #
 # for i in lijst:
-#     print(f' is nu {i}')
+#     print(f' is nu {i}') # altijd goed om even te checken wat je for-loop in de basis doet door te kijken wat i steeds is, en hoe vaak deze wordt uitgevoerd
 #     if i % 2 == 0:
 #         print(f'Dit getal is even: {i}')
